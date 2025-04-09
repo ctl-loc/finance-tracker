39906e17720f9e6102b06f194c60c017464779e9
Petrequin Simon <simon.petrequin@etudiant.univ-rennes1.fr>
2025-04-09 02:58:01 +0200

    [Added] frontend/src/app/app.routes.ts : Add login and register routes to app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,39 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+// import { LoginComponent } from "./components/login/login.component";
+// import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+// import { HomeComponent } from "./containers/home/home.component";
+import { authGuard } from "./guards/auth.guard";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    // redirect ro login
+    path: "",
+    title: "root",
+    redirectTo: "auth/login",
+    pathMatch: "full",
+  },
+  {
+    path: "auth/login",
+    title: "Login",
+    component: LoginComponent,
+    pathMatch: "full",
+  },
+  {
+    path: "auth/register",
+    title: "Register",
+    component: RegisterComponent,
+  },
+  // {
+  //   path: "dashboard",
+  //   title: "Dashboard",
+  //   component: HomeComponent,
+  //   canActivate: [authGuard],
+  // },
+  // {
+  //   path: "**",
+  //   title: "404 - Not Found",
+  //   component: PageNotFoundComponent,
+  // },
+];
