23f99c0eda5cf9edcfbb2fddf716d82621ba9a18
Petrequin Simon <simon.petrequin@etudiant.univ-rennes1.fr>
2025-04-12 01:31:12 +0200

    [UPDATED] frontend-dashboard : Replace custom card styles with Angular Material MatCard

--- frontend/src/app/containers/dashboard/dashboard.component.ts
+++ frontend/src/app/containers/dashboard/dashboard.component.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from "../../components/header/header.component";
 import { NewTransactionComponent } from "../../components/new-transaction/new-transaction.component";
 import { RecentTransactionComponent } from "../recent-transaction/recent-transaction.component";
 import { BankAccountComponent } from "../../components/bank-account/bank-account.component";
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: "app-dashboard",
@@ -15,6 +16,7 @@ import { BankAccountComponent } from "../../components/bank-account/bank-account
     NewTransactionComponent,
     RecentTransactionComponent,
     BankAccountComponent,
+    MatCardModule,
   ],
   templateUrl: "./dashboard.component.html",
   styleUrl: "./dashboard.component.css",
