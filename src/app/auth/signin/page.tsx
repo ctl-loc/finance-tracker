96245720337d18cd8f2ffbba736143690b3ac8bc
Petrequin Simon <simon.petrequin@etudiant.univ-rennes1.fr>
2025-04-28 17:21:00 +0200

    [ADDED] /auth/signin : added signin route

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,9 @@
+import SignInForm from "@/components/SigninForm";
+
+export default async function SignInPage() {
+    return (
+        <div className="bg-gray-800 h-screen flex items-center justify-center">
+            <SignInForm />
+        </div>
+    );
+}
