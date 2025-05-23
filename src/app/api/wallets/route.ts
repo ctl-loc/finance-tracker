28c00e0c777b685412cf2a570597847c1667402b
Petrequin Simon <simon.petrequin@etudiant.univ-rennes1.fr>
2025-05-23 04:31:57 +0200

    feat: add wallet API with GET and POST methods

---
+++ src/app/api/wallets/route.ts
@@ -0,0 +1,67 @@
+import { NextRequest, NextResponse } from "next/server";
+import { addWallet, getWallets } from "@/lib/wallets";
+import { BankAccount } from "@/generated/prisma";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const userId = searchParams.get("userId");
+
+  if (!userId) {
+    return NextResponse.json({ error: "Missing user_id" }, { status: 400 });
+  }
+
+  try {
+    const wallets = await getWallets(userId);
+
+    return NextResponse.json(wallets);
+  } catch (error) {
+    console.error("[ERROR] Prisma:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch wallets" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const { wallet } = await req.json();
+  console.log("wallet:", wallet);
+  // Validate wallet object
+  if (
+    !wallet ||
+    typeof wallet !== "object" ||
+    !wallet.userId ||
+    typeof wallet.userId !== "string" ||
+    !wallet.name ||
+    typeof wallet.name !== "string"
+  ) {
+    return NextResponse.json(
+      { error: "Missing or invalid wallet" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const walletTyped = {
+      userId: wallet.userId,
+      name: wallet.name,
+      balance: +wallet.baseAccountValue,
+    };
+
+    console.log(
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaawalletTyped:",
+      walletTyped.balance,
+      typeof walletTyped.balance
+    );
+
+    await addWallet(walletTyped);
+
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    console.error("[ERROR] Prisma:", error);
+    return NextResponse.json(
+      { error: "Failed to add new wallet" },
+      { status: 500 }
+    );
+  }
+}
