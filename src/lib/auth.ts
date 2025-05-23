9ad465ef7cdc6636562260c71b9fe0e8e8f1d2a9
Petrequin Simon <simon.petrequin@etudiant.univ-rennes1.fr>
2025-05-23 04:29:59 +0200

    refactor: improve code formatting and consistency in auth functions

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,45 +5,44 @@ import prisma from "@/lib/prisma";
 import bcrypt from "bcrypt";
 
 export async function login(
-    username: string,
-    password: string,
+  username: string,
+  password: string
 ): Promise<{ user?: User; error?: string }> {
-    try {
-        const user = await prisma.user.findUnique({
-            where: { email: username },
-        });
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: username },
+    });
 
-        if (!user) {
-            return { error: "User not found" };
-        }
-        console.log(user);
-        if (!(await bcrypt.compare(password, user.password as string))) {
-            return { error: "Invalid password" };
-        }
-
-        return { user };
-    } catch (error) {
-        console.error(error);
-        return { error: "Internal server error" };
+    if (!user) {
+      return { error: "User not found" };
     }
+    if (!(await bcrypt.compare(password, user.password as string))) {
+      return { error: "Invalid password" };
+    }
+
+    return { user };
+  } catch (error) {
+    console.error(error);
+    return { error: "Internal server error" };
+  }
 }
 
 export async function register(
-    username: string,
-    password: string,
+  username: string,
+  password: string
 ): Promise<void> {
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        await prisma.user.create({
-            data: {
-                email: username,
-                password: hashedPassword,
-            },
-        });
+    await prisma.user.create({
+      data: {
+        email: username,
+        password: hashedPassword,
+      },
+    });
 
-        console.log("registration successful");
-    } catch (error) {
-        console.error(error);
-    }
+    console.log("[INFO] Registration successful");
+  } catch (error) {
+    console.error(error);
+  }
 }
