496df7be9c7f7633d42b643da4419813ec2b0fbf
Petrequin Simon <simon.petrequin@etudiant.univ-rennes1.fr>
2025-05-26 00:01:16 +0200

    feat: add WalletCardComponent for displaying wallet information

---
+++ src/components/wallet/WalletCardComponent.tsx
@@ -0,0 +1,21 @@
+import { BankAccount } from "@/generated/prisma";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+
+export default function WalletCardComponent({
+  wallet,
+}: {
+  wallet: BankAccount;
+}) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{wallet.name}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="text-3xl text-blue-900 font-bold">
+          €{wallet.balance}
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
