eb9bf649548c73b56070a98e0188cd96a414feec
Petrequin Simon <simon.petrequin@etudiant.univ-rennes1.fr>
2025-05-26 00:01:07 +0200

    refactor: replace individual button components with dynamic rendering from buttonsValue array

--- src/components/NavBar/NavBarComponent.tsx
+++ src/components/NavBar/NavBarComponent.tsx
@@ -1,34 +1,53 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { Button } from "../ui/button";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+const buttonsValue = [
+  {
+    name: "Dashboard",
+    action: (router: AppRouterInstance) => {
+      router.push("/dashboard");
+    },
+  },
+  {
+    name: "Transaction",
+    action: (router: AppRouterInstance) => {
+      router.push("/transactions");
+    },
+  },
+  {
+    name: "Wallet",
+    action: (router: AppRouterInstance) => {
+      router.push("/wallets");
+    },
+  },
+  {
+    name: "Goals",
+    action: (router: AppRouterInstance) => {
+      router.push("/goals");
+    },
+  },
+];
 
 export default function NavBarComponent() {
   const router = useRouter();
-
-  const onWalletClick = () => {
-    router.push("/wallet");
-  };
 
   return (
     <div className="flex flex-col gap-2 bg-purple-600 h-full w-full p-4">
       <div>Logo</div>
       <div className="flex flex-col gap-2">
-        <Button variant="outline" className="bg-purple-800 text-white">
-          Dashboard
-        </Button>
-        <Button variant="outline" className="bg-purple-800 text-white">
-          Transactions
-        </Button>
-        <Button
-          variant="outline"
-          className="bg-purple-800 text-white"
-          onClick={onWalletClick}
-        >
-          Wallet
-        </Button>
-        <Button variant="outline" className="bg-purple-800 text-white">
-          Goals
-        </Button>
+        {/* each button */}
+        {buttonsValue.map(({ name, action }) => (
+          <Button
+            variant="outline"
+            className="bg-purple-800 text-white"
+            onClick={() => action(router)}
+            key={name}
+          >
+            {name}
+          </Button>
+        ))}
       </div>
     </div>
   );
