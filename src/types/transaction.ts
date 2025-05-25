b3b7ccc0189276696f574259cbf53ecce16dbac9
Petrequin Simon <simon.petrequin@etudiant.univ-rennes1.fr>
2025-05-26 00:01:58 +0200

    refactor: simplify TransactionClient type by using Omit for properties

--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -1,11 +1,7 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export type TransactionClient = {
-  userId: string;
-  amount: number;
-  description: string;
-  tags: string[];
-  bankAccount?: any;
-  createdAt?: any;
-  updatedAt?: any;
-  id?: string;
-};
+import { Transaction } from "@/generated/prisma";
+
+// Replace 'YourType' with the actual type and 'keyToOmit' with the property you want to omit
+export type TransactionClient = Omit<
+  Transaction,
+  "id" | "createdAt" | "updatedAt"
+>;
