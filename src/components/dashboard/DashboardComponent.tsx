e638608cd10cebd0ce928452dcd0ce46e4f216bb
ctl-loc <simon.petrequin@free.fr>
2025-06-09 00:56:24 +0000

    refactor:  move total balance logic to TotalComponent

--- src/components/dashboard/DashboardComponent.tsx
+++ src/components/dashboard/DashboardComponent.tsx
@@ -1,59 +1,18 @@
-import useWallet from "@/hooks/wallet";
 import BudgetComponent from "./BudgetComponent";
 import DashboardCardComponent from "./CardComponent";
 import GoalsComponent from "./GoalsComponent";
 import MoneyFlowComponent from "./MoneyFlowComponent";
 import RecentTransactions from "./RecentTransactions";
-import { startTransition, useEffect, useState } from "react";
+import TotalComponent from "./TotalComponent";
 
 export default function DashboardComponent() {
-  const { wallets, getWallets } = useWallet();
-
-  const [totalAmount, setTotalAmount] = useState(0);
-  const [totalAmountOld, setTotalAmountOld] = useState(0);
-
-  useEffect(
-    () =>
-      startTransition(async () => {
-        if (!wallets) return;
-        const total = wallets.reduce((acc, curr) => acc + curr.balance, 0);
-        setTotalAmount(total);
-
-        // total last month calculation
-        const fetchOldBalances = async () => {
-          // for each wallet, fetch its balance 1month ago
-          const balances = await Promise.all(
-            wallets.map(async (curr) => {
-              const walletList = await getWallets(
-                curr.id,
-                new Date(new Date().setMonth(new Date().getMonth() - 1))
-              ); // one month ago
-
-              return walletList[0]?.balance;
-            })
-          );
-
-          const totalOld = balances.reduce((acc, balance) => acc + balance, 0);
-          console.log(totalAmountOld);
-          setTotalAmountOld(totalOld);
-        };
-        fetchOldBalances();
-      }),
-    [wallets]
-  );
-
   return (
     <div className="flex flex-col justify-start h-full w-full">
       <div className="flex flex-col h-full w-full flex-1">
         {/* top bar */}
         <div className="flex flex-row w-full p-2 gap-2 flex-1">
           <div className="flex-1">
-            <DashboardCardComponent
-              title={"Total Balance"}
-              amount={totalAmount}
-              growth={((totalAmount - totalAmountOld) * 100) / totalAmountOld} // calculate percentage
-              periodGrowth={"last month"}
-            />
+            <TotalComponent />
           </div>
           <div className="flex-1">
             <DashboardCardComponent
---
+++ src/components/dashboard/TotalComponent.tsx
@@ -0,0 +1,61 @@
+import { startTransition, useEffect, useState } from "react";
+import DashboardCardComponent from "./CardComponent";
+import { BankAccount } from "@/generated/prisma";
+import { useSession } from "next-auth/react";
+import { getHistoryWallet, getWallets } from "@/actions/wallets";
+
+export default function TotalComponent() {
+  const { data: session, status } = useSession();
+  const [wallets, setWallets] = useState([] as BankAccount[]);
+  const [currentTotal, setCurrentTotal] = useState(0);
+  const [oldTotal, setOldTotal] = useState(0);
+
+  useEffect(
+    () =>
+      startTransition(async () => {
+        if (status !== "authenticated") return;
+        const fetchedWallets = await getWallets(session.user.id);
+        if (fetchedWallets.success) setWallets(fetchedWallets.data);
+      }),
+    [session, status]
+  );
+
+  useEffect(
+    () =>
+      startTransition(async () => {
+        if (!wallets) return;
+        if (status !== "authenticated") return;
+
+        // Current total calculation
+        const currTotal = wallets.reduce((acc, curr) => acc + curr.balance, 0);
+        setCurrentTotal(currTotal);
+
+        // Old total calculation
+        const oneMonthAgo = new Date(
+          new Date().setMonth(new Date().getMonth() - 1)
+        );
+        const oldBalances = await Promise.all(
+          wallets.map(async (curr) => {
+            const history = await getHistoryWallet(
+              session.user.id,
+              curr.id,
+              oneMonthAgo
+            );
+            return history.data?.balance ?? curr.balance;
+          })
+        );
+        const old = oldBalances.reduce((acc, balance) => acc + balance, 0);
+        setOldTotal(old);
+      }),
+    [wallets, status, session]
+  );
+
+  return (
+    <DashboardCardComponent
+      title={"Total Balance"}
+      amount={currentTotal}
+      growth={((currentTotal - oldTotal) * 100) / oldTotal} // calculate percentage
+      periodGrowth={"last month"}
+    />
+  );
+}
