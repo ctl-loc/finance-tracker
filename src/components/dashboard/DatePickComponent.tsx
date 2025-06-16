ece1b089899b7cb88050ebdcd406fcabd4825b13
ctl-loc <simon.petrequin@free.fr>
2025-06-16 12:35:36 +0000

    feat: prevent datePicker to select the future

--- src/components/dashboard/DatePickComponent.tsx
+++ src/components/dashboard/DatePickComponent.tsx
@@ -7,40 +7,44 @@ import { Calendar as CalendarIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
 } from "@/components/ui/popover";
 import { Label } from "../ui/label";
 
 export function DatePickerComponent({
-  date,
-  setter,
+    date,
+    setter,
 }: {
-  date: Date | undefined;
-  setter: React.Dispatch<React.SetStateAction<Date | undefined>>;
+    date: Date | undefined;
+    setter: React.Dispatch<React.SetStateAction<Date | undefined>>;
 }) {
-  return (
-    <div className="flex flex-col">
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            data-empty={!date}
-            className="justify-start text-left font-normal"
-          >
-            <CalendarIcon color={date ? "#01a52b" : undefined} />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0">
-          <Calendar
-            mode="single"
-            selected={date}
-            onSelect={setter}
-            required={false}
-          />
-        </PopoverContent>
-      </Popover>
-    </div>
-  );
+    return (
+        <div className="flex flex-col">
+            <Popover>
+                <PopoverTrigger asChild>
+                    <Button
+                        variant="outline"
+                        data-empty={!date}
+                        className="justify-start text-left font-normal"
+                    >
+                        <CalendarIcon color={date ? "#01a52b" : undefined} />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                    <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={(date) => {
+                            // set date if it is in the past
+                            if (date && date > new Date()) return;
+                            setter(date);
+                        }}
+                        required={false}
+                    />
+                </PopoverContent>
+            </Popover>
+        </div>
+    );
 }
