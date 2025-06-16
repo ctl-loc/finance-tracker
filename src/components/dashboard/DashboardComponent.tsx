bbc0764978285e615230f2fa5c70c21959ed0b64
ctl-loc <simon.petrequin@free.fr>
2025-06-16 00:12:45 +0000

    feat: integrate date selection and update components to use selected date

--- src/components/dashboard/DashboardComponent.tsx
+++ src/components/dashboard/DashboardComponent.tsx
@@ -1,42 +1,40 @@
+import { useState } from "react";
+import { Separator } from "../ui/separator";
 import BudgetComponent from "./BudgetComponent";
 import DashboardCardComponent from "./CardComponent";
+import { DatePickerComponent } from "./DatePickComponent";
 import GoalsComponent from "./GoalsComponent";
+import IncomeComponent from "./IncomeComponent";
 import MoneyFlowComponent from "./MoneyFlowComponent";
 import RecentTransactions from "./RecentTransactions";
 import TotalComponent from "./TotalComponent";
+import { getDashboardDates } from "@/lib/dates";
+import ExpensesComponent from "./ExpensesComponent";
+import SavingsComponent from "./SavingsComponent";
 
 export default function DashboardComponent() {
+  const [date, setDate] = useState<Date>();
   return (
     <div className="flex flex-col justify-start h-full w-full">
       <div className="flex flex-col h-full w-full flex-1">
+        {/* header ? */}
+        <div className="flex flex-row bg-amber-200">
+          <DatePickerComponent date={date} setter={setDate} />
+        </div>
+        <Separator />
         {/* top bar */}
         <div className="flex flex-row w-full p-2 gap-2 flex-1">
           <div className="flex-1">
-            <TotalComponent />
+            <TotalComponent dates={getDashboardDates(date)} />
           </div>
           <div className="flex-1">
-            <DashboardCardComponent
-              title={"Incomes"}
-              amount={200}
-              growth={-2}
-              periodGrowth={"last month"}
-            />
+            <IncomeComponent dates={getDashboardDates(date)} />
           </div>
           <div className="flex-1">
-            <DashboardCardComponent
-              title={"Expenses"}
-              amount={200}
-              growth={-2}
-              periodGrowth={"last month"}
-            />
+            <ExpensesComponent dates={getDashboardDates(date)} />
           </div>
           <div className="flex-1">
-            <DashboardCardComponent
-              title={"Total Savings"}
-              amount={200}
-              growth={-2}
-              periodGrowth={"last month"}
-            />
+            <SavingsComponent dates={getDashboardDates(date)} />
           </div>
         </div>
         {/* middle bar */}
